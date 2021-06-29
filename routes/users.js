11747d12bcc7c0315860a5ae86feaba52748f0f4
Biswanath Tewari <40662234+biZan7X@users.noreply.github.com>
2021-06-29 11:55:24 +0530

    feat - lodash

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const _ = require("lodash");
 const { User, validateUser } = require("../models/users");
 
 router.post("/", async (req, res) => {
@@ -10,14 +11,10 @@ router.post("/", async (req, res) => {
 	const userTemp = await User.findOne({ email: req.body.email });
 	if (userTemp) return res.status(400).send("the user already exists");
 
-	const user = new User({
-		name: req.body.name,
-		email: req.body.email,
-		password: req.body.password,
-	});
+	const user = new User(_.pick(req.body, ["name", "email", "password"]));
 
 	await user.save();
-	res.send(user);
+	res.send(_.pick(user, ["_id", "name", "email"]));
 });
 
 module.exports = router;
