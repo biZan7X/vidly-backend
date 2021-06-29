4ef1262f93b680f978d5d3dcf73bfad6c157a1e2
Biswanath Tewari <40662234+biZan7X@users.noreply.github.com>
2021-06-29 12:21:09 +0530

    feat - hashing passwords

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const _ = require("lodash");
+const bcrypt = require("bcrypt");
 const { User, validateUser } = require("../models/users");
 
 router.post("/", async (req, res) => {
+	//& express validation
 	const { error } = validateUser(req.body);
 	if (error) return res.status(400).send(error.details[0].message);
 
@@ -12,6 +14,10 @@ router.post("/", async (req, res) => {
 	if (userTemp) return res.status(400).send("the user already exists");
 
 	const user = new User(_.pick(req.body, ["name", "email", "password"]));
+
+	//& hasing the passwords
+	const salt = await bcrypt.genSalt(10);
+	user.password = await bcrypt.hash(user.password, salt);
 
 	await user.save();
 	res.send(_.pick(user, ["_id", "name", "email"]));
