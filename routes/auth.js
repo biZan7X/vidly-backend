651a44134df79407f974d9ff6b53b8953c063957
Biswanath Tewari <40662234+biZan7X@users.noreply.github.com>
2021-06-30 22:57:44 +0530

    feat - auth route

---
+++ routes/auth.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const _ = require("lodash");
+const bcrypt = require("bcrypt");
+const Joi = require("joi");
+const { User } = require("../models/users");
+
+router.post("/", async (req, res) => {
+	//& express validation
+	const { error } = validateAuth(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
+
+	//& checking if the email already exists
+	const userTemp = await User.findOne({ email: req.body.email });
+	if (userTemp) return res.status(400).send("invalid email or password");
+
+	//& checking the passwords
+	const validPassword = await bcrypt.compare(req.body.password, user.password);
+	if (!validPassword) return res.status(400).send("Invalid email or password");
+
+	res.send(true);
+});
+
+const validateAuth = (auth) => {
+	const schema = Joi.object({
+		email: Joi.string().required().min(5).max(255).email(),
+		password: Joi.string().required().min(5).max(255),
+	});
+
+	return schema.validate(user);
+};
+
+module.exports = router;
